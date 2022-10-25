4f9386021e0eac9e5dd98be8781d6863612512f2
Marharyta Bosak <aboimova.rita@gmail.com>
2022-10-25 13:26:31 +0300

    add superheroes selectors

--- src/views/SuperheroInfoView/SuperheroInfoView.js
+++ src/views/SuperheroInfoView/SuperheroInfoView.js
@@ -1,7 +1,13 @@
 import SuperheroInfo from "../../components/SuperheroInfo";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getSuperheroById } from "../../redux/superheroes/superheroesSelectors";
 
 const SuperheroInfoView = () => {
-  return <SuperheroInfo />;
+  const { id } = useParams();
+  const superheroById = useSelector((state) => getSuperheroById(state, id));
+
+  return <SuperheroInfo superhero={superheroById} />;
 };
 
 export default SuperheroInfoView;
