bf94df19bd1d29f1a3ee6e5ffa027402d7a25995
Marharyta Bosak <aboimova.rita@gmail.com>
2022-10-25 13:30:49 +0300

    add navigation to superhero info by id

--- src/components/SuperheroesList/SuperheroesList.js
+++ src/components/SuperheroesList/SuperheroesList.js
@@ -1,20 +1,33 @@
 import { SyncLoader } from "react-spinners";
+import { useNavigate } from "react-router-dom";
 
 import SuperheroesListItem from "../SuperheroesListItem";
 import { useGetSuperheroesQuery } from "../../redux/superheroes/superheroesReducer";
 
 const SuperheroesList = () => {
   const { data: superheroes, isFetching } = useGetSuperheroesQuery();
+  const navigate = useNavigate();
+
+  const navigateToSuperheroById = (id) => {
+    navigate(`/${id}`);
+  };
 
   return (
     <div>
       {isFetching && <SyncLoader color="#757b7a" />}
+
       {superheroes && (
         <ul>
           {superheroes.map((superhero) => {
-            const { _id } = superhero;
+            const { _id: id } = superhero;
 
-            return <SuperheroesListItem key={_id} superhero={superhero} />;
+            return (
+              <SuperheroesListItem
+                key={id}
+                superhero={superhero}
+                onSuperheroClick={navigateToSuperheroById}
+              />
+            );
           })}
         </ul>
       )}
